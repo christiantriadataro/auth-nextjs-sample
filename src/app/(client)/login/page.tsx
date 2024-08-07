fdfe1694c1852f8c3209dd6b8659ee75719fb32e
Christian Dataro <cdataro@sbay.southbaylogistics.com>
2024-08-07 11:46:21 +0800

    Signup and Login Working

--- src/app/(client)/login/page.tsx
+++ src/app/(client)/login/page.tsx
@@ -1,26 +1,54 @@
 "use client";
 import Link from "next/link"
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
 import axios from "axios"
+import toast from "react-hot-toast";
 
 const LoginPage = () => {
+    const router = useRouter();
     const [user, setUser] = useState({
         password: "",
         username: ""
     })
 
-    const onLogin = async () => {
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [loading,setLoading] = useState(false);
 
+    const onLogin = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login success", response.data)
+            toast.success("Login success");
+            router.push("/profile")
+        } catch (error: any) {
+            console.log("Login failed", error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
     }
 
+    // @ts-ignore
     const handleEmailChange = event => setUser({...user, email: event.target.value})
+    // @ts-ignore
     const handlePasswordChange = event => setUser({...user, password: event.target.value})
+    // @ts-ignore
     const handleUsernameChange = event => setUser({...user, username: event.target.value})
+
+
+    useEffect(() => {
+        if (user.username.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user]);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>SignupPage</h1>
+            <h1>{loading ? "Processing" : "Login"}</h1>
             <hr/>
             <label htmlFor="username">username</label>
             <input
@@ -42,7 +70,8 @@ const LoginPage = () => {
             />
             <button
                 onClick={onLogin}
-                className="p-2 border border-gray-300 rounded-lg mb-4"
+                className={`p-2 border border-gray-300 rounded-lg mb-4 ${buttonDisabled ? "text-rose-500" : "text-green-500"}`}
+                disabled={buttonDisabled}
             >
                 Login
             </button>
--- src/app/(server)/api/users/signup/route.ts
+++ src/app/(server)/api/users/signup/route.ts
@@ -2,7 +2,6 @@ import {connect} from "@/helpers/databaseConfig";
 import User from "@/models/userModel"
 import {NextRequest, NextResponse} from "next/server";
 import bcryptjs from "bcryptjs"
-import {bgCyan} from "next/dist/lib/picocolors";
 
 
 connect()
@@ -23,7 +22,22 @@ export const POST = async (request: NextRequest) => {
 
         // hash password
         const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt);
 
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+        })
+
+        const savedUser = await newUser.save()
+        console.log(savedUser);
+
+        return NextResponse.json({
+            message: "User created Successfully",
+            success: true,
+            savedUser
+        })
         // basic validation zod?
 
     } catch (error: any) {
